ba48c2ec294940b0a81acfa7b36d7a856ac43c15
Jonathan <squiidz@BxNix.(none)>
2015-02-15 03:41:26 -0500

    Update ref

--- setup.sh
+++ setup.sh
@@ -1,7 +1,7 @@
 # Install Vundle
 git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 # Get the .vimrc file
-mv vimrc ~/.vimrc
+mv /vimConf/vimrc ~/.vimrc
 # Install Plugin
 vim +PluginInstall +qall
 
