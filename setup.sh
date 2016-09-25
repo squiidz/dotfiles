b0903b7c2ce4c8feea5a9e2bbb2218b925f17caa
CodingFerret <piinkyz05@gmail.com>
2016-09-25 14:38:36 -0400

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-# Install Vundle
-git clone https://github.com/gmarik/Vundle.vim.git ~/.vim/bundle/Vundle.vim
-# Get the .vimrc file
-mv ~/vimConf/vimrc ~/.vimrc
-# Install Plugin
-vim +PluginInstall +qall
-
-
